b6d43568510ff18083a02bb9869bcfcfde978e95
Kene323 <ezemma789@gmail.com>
2025-03-12 13:40:53 -0500

    update

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.use(morgan("dev"))
 app.use("/api", detailroute)
 app.use("/api", userroute)
 
+app.all("/", (req, res) => {
+    return res.status(200).json({message: "API is up and running"})
+})
+
+app.all("*", (req, res) => {
+    return res.status(404).json({message: "The route does not exist"})
+})
+
 app.listen(port, () => {
     console.log(new Date().toLocaleString(), port);
 })
